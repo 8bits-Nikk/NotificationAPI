5c4bc95f7e086f862e54a9f4915adf5068627b40
8bits-Nikk <nikhilmakwana028@gmail.com>
2022-03-24 13:52:52 +0530

    New Files

---
+++ key/serviceAccount.js
@@ -0,0 +1,13 @@
+export default {
+    "type": "service_account",
+    "project_id": "mylibrary-f51d8",
+    "private_key_id": "ab9d95620f62793b263f62aa17ef10d9d4bdfe41",
+    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDK+mYrFWKXwptX\nSFtPymfggbi++OOEr6i+oHjyKpPgnwTEgdU86hSzpn02FHxso0GkAo5uTky+pHkO\nAwy4AuwQJAkCPYTJybsAoTcnS2MQ7J/ZSvtsOxljPpg/ieP4vwH8rj/1gN2EZr1Z\nQHdmkQJWlmjnvftTghevRNcw+Y+7aPGrj6y+7TukHgQwv+7f80/CIkS6uCaVmszS\nsAttg3bVznbTyb3BDiDuz24bYeQKHOaaf/kXTIhvZ/LbRv9TiIHR0M/Ok20+UIJW\nbKiR/zPrRunUtB2EBsiuwjtga2Pis7H0By2fb7Rrq5gO0rdodgd5iQQWyQG34RwK\nSP3FbrKvAgMBAAECggEAUjvrW6Yg2Ro7ZDvI3sVCm4cCycA9MTsXkw+hoH8WhUj3\nngN52GF/vJ8FmHzpQfxMcj2k5dSsLAqnjsE2jHCGoi1vRuyUmUo1DJbEKRPWUXuE\ncXz66sjwyrAH76oWZedAz1XrTLQGSwdyzC+dkDzMpTbvzn++TIFmxhgOVOWu0T6E\nUKwdpj5eaVAUQH4eBZQKDHTyC3X9G6QaIZ64Oeagt3UVt334eC6ZFZNyWKF2yl9l\nN+nqowscyhEVnsSpvt6pgT+5MPNzkXcZohnpzRziezDN++sFflmq+OI/rQzJyr9z\nxn59/SAQjsjnsxk3bRvB8I8uDig2phOsiN2qMlq8NQKBgQD4jMee0puF2yADz2Jo\nlhKwkXPD362pe1AFB5yTesotc4FpItAxQ5ciHHy8V6iDD7n1zt2V7enyBJ0GwCRE\nljK4tNbKFeodL4DadxWd6FAMx2+9/r+RWPc9MNx6/Rbjf5i7/iOHZ07d5Qy6H/Fx\nyfuA93yhIeNgwEacjGOXdSjlvQKBgQDRD+3e4MrcUi2o1Ce25SBj8ngSSoC5M96O\nI5GGWz2VUhrd+CIT6kPoFvigtS56MA4Mxv7SPk4Yizf91WUbcKppVrzNp0zb3gSc\ni98zqyDu13xQ/hQycvxdU735b0k7Ya5KJaHPv94aA5ObC1GcfLKGCsovmh0QtMip\nPpjIIfRy2wKBgQDoxO8uDo8YDN8uskm3KNTYB2AjUJeS4alPDJHWjGhLqKDJlZDk\nkig4CfXHbVCjbVb6nYFnqCgwP6HPcKKGmSNnV/TPzTYznwlNj7ju6WIOlbhQdwNx\nW6Av7lKN2/hTlmuzo44igN2bcac5qDC2IDMp1J0jSllngqqecdvTPCXRAQKBgHxy\nowKdhaSJNqzdPZbd5d4ZI+ifeOjGO6EZAzObdJlCesPPRVndhfbOVco0kCokGPj6\nLA4zEIIDvZzYPouLEaYK//DbuiDXlJ8rk/5JzBzhIRuFY7h0Qk2738ZvvIQgrYNt\ny1jDug56Oo98uWQRxqOMILcdYlfjhd9ZZ9EahU0pAoGAe225fNtdVIvCVtlMwAkD\nBu6yUbLAY6GLl9NghKSBV1Z7gJ9JSTu0kvdIcBy1QJTSw1GQ5qeZwkX8JkNHPQ+l\niszkCDFCBmY9MHfpbAxNU878ytyM4xHP7XBJ1c8vGWWVwFGBiczeTuTaafskxoAc\nr6chzyOjZG1bmskZaDXRdVk=\n-----END PRIVATE KEY-----\n",
+    "client_email": "firebase-adminsdk-za7cm@mylibrary-f51d8.iam.gserviceaccount.com",
+    "client_id": "113735149875845713808",
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-za7cm%40mylibrary-f51d8.iam.gserviceaccount.com"
+}
+
